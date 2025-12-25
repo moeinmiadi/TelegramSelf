b9fa06241a1b6a4debba5b8753a2375a08fccbfe
Mohammad Moein Miadi <miadimoein1386@gmail.com>
2025-12-25 20:49:19 +0330

    Update Information.py

--- lib/Information.py
+++ lib/Information.py
@@ -1,10 +1,10 @@
 from .library import *
 
-admin_user_id = 123456789 #<--- آیدی ادمین
-api_id = 123456789 #<--- آی پی آی آیدی
-api_hash = 'abcdefghigklmnopqrstuvwxyz' #<--- ای پی آی هش
-helper_username = 'BotUserName' #<--- یوزر ربات هلپر بدون @
-bot_token = 'abcdefghigklmnopqrstuvwxyz:466546' #<--- توکن ربات هلپر
+admin_user_id = 1868180475 #<--- آیدی ادمین
+api_id = 11795206 #<--- آی پی آی آیدی
+api_hash = '53768e7ecd4406e4d88464d71a30165a' #<--- ای پی آی هش
+helper_username = 'sohancoin_bot' #<--- یوزر ربات هلپر بدون @
+bot_token = '7723117220:AAHD5dmGJvY9NMKZ8xyVWqk-eJHGTFJsu4A' #<--- توکن ربات هلپر
 
 client_id = '01e7dc6b41c3471b94efe87abeb05919'
 client_secret = '4f5f93af1ced4b0d9ba8440606803639'
